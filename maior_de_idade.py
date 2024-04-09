0b25c75d02068e2f84d8fcc1b2bd67468d2802b2
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-09 09:24:13 -0300

    Add files via upload

---
+++ maior_de_idade.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print(f"{nome}, você tem {idade} por tanto você é maior de idade.")
+
+else:
+    print(f"{nome}, você tem {idade} por tanto você não é maior de idade.")
+
+

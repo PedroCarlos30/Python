13270ae16187f99f1bca1cc7d7d9e51ce8572619
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-16 10:33:22 -0300

    Update maior_de_idade.py

--- maior_de_idade.py
+++ maior_de_idade.py
@@ -1,11 +1,9 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
-if idade >= 18 && idade <= 59:
+if idade >= 18 and idade <= 59:
     print(f"{nome}, você tem {idade} por tanto você é maior de idade.")
-else if(idade >= 60):
-    print(f"Você é idoso.")
-else:
+if idade >= 60:
+    print(f"{nome} você tem {idade} por tanto você é idoso.")
+if idade < 18:
     print(f"{nome}, você tem {idade} por tanto você não é maior de idade.")
-
-
 
